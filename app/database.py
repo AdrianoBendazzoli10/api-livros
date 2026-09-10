a8174ce741692e4dc0b0cc6ef841da3d8c6a145e
Adrianobendazzoli10 <dri431784@gmail.com>
2026-09-10 09:11:49 -0300

    feat: cria sessao do banco

--- app/database.py
+++ app/database.py
@@ -1,10 +1,11 @@
+from collections.abc import Generator
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 
 
 class Configuracoes(BaseSettings):
-    
     db_user: str
     db_password: str
     db_host: str = "localhost"
@@ -27,3 +28,12 @@ criar_sessao = sessionmaker(bind=mecanismo_banco, autoflush=False, autocommit=Fa
 
 class BaseBanco(DeclarativeBase):
     pass
+
+
+def obter_sessao_banco() -> Generator[Session, None, None]:
+    sessao_banco = criar_sessao()
+
+    try:
+        yield sessao_banco
+    finally:
+        sessao_banco.close()
