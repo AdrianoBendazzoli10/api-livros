9e76051d40cc8f45d16e380800028ffc99c7a668
Adrianobendazzoli10 <dri431784@gmail.com>
2026-09-17 09:34:57 -0300

    docs: atualiza banco apos conclusao do crud

--- biblioteca_db.sql
+++ biblioteca_db.sql
@@ -3,16 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 03-Set-2026 às 15:07
+-- Tempo de geração: 17-Set-2026 às 14:34
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
-
-
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -25,6 +22,52 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `biblioteca_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `biblioteca_db`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livros`
+--
+
+CREATE TABLE `livros` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `autor` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ano_publicacao` int(11) NOT NULL,
+  `disponivel` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Extraindo dados da tabela `livros`
+--
+
+INSERT INTO `livros` (`id`, `titulo`, `autor`, `ano_publicacao`, `disponivel`) VALUES
+(1, 'Dom Casmurro', 'Machado de Assis', 1899, 0),
+(2, 'O Cortiço', 'Aluísio Azevedo', 1890, 0),
+(3, 'Memórias Póstumas de Brás Cubas', 'Machado de Assis', 1881, 0),
+(4, 'Vidas Secas', 'Graciliano Ramos', 1938, 0),
+(5, 'Capitães da Areia', 'Jorge Amado', 1937, 0);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ix_livros_id` (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `livros`
+--
+ALTER TABLE `livros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
